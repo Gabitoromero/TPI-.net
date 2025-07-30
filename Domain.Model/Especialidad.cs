96843d36afe483e5486d1ad2547772f05367a25c
Gabitoromero <garomero@frro.utn.edu.ar>
2025-07-30 09:12:03 -0300

    creo entidad especialidad

---
+++ Domain.Model/Especialidad.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model
+{
+    internal class Especialidad
+    {
+        //Fields
+        int _id;
+        string _descripcion;
+        //Properties
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
