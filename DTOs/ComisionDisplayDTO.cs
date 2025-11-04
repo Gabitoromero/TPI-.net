b1dac194216be99ccb28e5c3286e0ee123615762
Gabitoromero <garomero@frro.utn.edu.ar>
2025-11-04 09:33:38 -0300

    nuevo DTO para mostrar comisiones

---
+++ DTOs/ComisionDisplayDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs
+{
+    public class ComisionDisplayDTO
+    {
+        public int Id_comision { get; set; }
+        public string Desc_comision { get; set; } 
+        public int Anio_especialidad { get; set; }
+        public int Id_plan { get; set; }
+        public bool Habilitado { get; set; }
+        public string PlanDescripcion { get; set; } 
+    }
+}
